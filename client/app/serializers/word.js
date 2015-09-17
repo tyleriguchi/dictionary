eb0ac127b94b28e0d269ac0df4e47742791133a8
tyleriguchi <tyleriguchi@gmail.com>
2015-09-16 18:33:01 -0700

    push definitions on response from server into store

---
+++ client/app/serializers/word.js
@@ -0,0 +1,12 @@
+import DS from 'ember-data';
+
+export default DS.JSONAPISerializer.extend({
+  normalizeSingleResponse: function(store, primaryModelClass, payload, id, requestType) {
+    // push in the definitions array from the response from the server
+    payload.data.forEach( (definition) => {
+      store.push(definition);
+    })
+
+    return this._super(...arguments);
+  },
+});

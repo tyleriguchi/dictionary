183a292bdf6a4e7a760dc31e4c49fe4fcc6b532b
tyleriguchi <tyleriguchi@gmail.com>
2015-09-19 12:14:32 -0700

    attempts to create ember data relationships

--- client/app/models/word.js
+++ client/app/models/word.js
@@ -2,4 +2,5 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   word: DS.attr('string'),
+  definitions: DS.hasMany('definition')
 });
--- client/app/serializers/word.js
+++ client/app/serializers/word.js
@@ -2,10 +2,10 @@ import DS from 'ember-data';
 
 export default DS.JSONAPISerializer.extend({
   normalizeSingleResponse: function(store, primaryModelClass, payload, id, requestType) {
-    // push in the definitions array from the response from the server
-    payload.data.forEach( (definition) => {
-      store.push(definition);
-    })
+    // // push in the definitions array from the response from the server
+    // payload.data.forEach( (definition) => {
+    //   store.push(definition);
+    // })
 
     return this._super(...arguments);
   },

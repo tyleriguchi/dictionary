4b7766dac329572d81e5f6ba658bb61a0171b663
tyleriguchi <tyleriguchi@gmail.com>
2015-10-04 17:59:22 -0700

    update relationship to include a user

--- client/app/word/model.js
+++ client/app/word/model.js
@@ -3,6 +3,7 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   word: DS.attr('string'),
   definitions: DS.attr(),
+  user: DS.belongsTo('user'),
   isAuthenticated: DS.attr('boolean', {
     defaultValue: false
   }),

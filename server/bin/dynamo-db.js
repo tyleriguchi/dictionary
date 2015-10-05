415aeccb8bfba4e97ceb2a0edb671c39a22bfd1b
tyleriguchi <tyleriguchi@gmail.com>
2015-10-04 18:00:23 -0700

    this file will be deleted

--- server/bin/dynamo-db.js
+++ server/bin/dynamo-db.js
@@ -6,48 +6,54 @@ localDynamo.launch({
 });
 
 
-setTimeout(function() {
-  var dynamoDbOptions = {
-    apiVersion: '2012-08-10',
-    endpoint: 'http://localhost:4567',
-    region: 'us-west-2'
-  };
+var dynamoDbOptions = {
+  apiVersion: '2012-08-10',
+  endpoint: 'http://localhost:4567',
+  region: 'us-west-2'
+};
 
-  var dynamoDb = new SDK.DynamoDB(dynamoDbOptions);
+var dynamoDb = new AWS.DynamoDB(dynamoDbOptions);
 
-  dynamoDb.listTables({}, function(err, data) {
-    if (err) {
-      return console.log(err, err.stack);
-    }
+dynamoDb.listTables({}, function(err, data) {
+  if (err) {
+    return console.log(err, err.stack);
+  }
 
-    var tables = data.TableNames;
+  var tables = data.TableNames;
 
-    if (tables.indexOf('dictionary') < 0) {
-      var params = {
-        KeySchema: [
-          {
-            AttributeName: 'id',
-            KeyType: 'HASH'
-          }
-        ],
-        AttributeDefinitions: [{
+  if (tables.indexOf('dictionary') < 0) {
+    var params = {
+      KeySchema: [
+        {
           AttributeName: 'id',
-          AttributeType: 'S'
-        }],
-        ProvisionedThroughput: {
-          ReadCapacityUnits: 1,
-          WriteCapacityUnits: 1
-        },
-        TableName: 'dictionary'
-      };
+          KeyType: 'HASH'
+        }
+      ],
+      AttributeDefinitions: [{
+        AttributeName: 'id',
+        AttributeType: 'S'
+      }],
+      ProvisionedThroughput: {
+        ReadCapacityUnits: 1,
+        WriteCapacityUnits: 1
+      },
+      TableName: 'dictionary'
+    };
 
-      dynamoDb.createTable(params, function(err, result) {
-        if (err) return console.log(err, err.stack);
+    dynamoDb.createTable(params, function(err, result) {
+      if (err) return console.log(err, err.stack);
 
-        console.log('success', result);
-      });
-    }
+      console.log('success', result);
+    });
+  }
+  else {
+    var params = {
+      TableName: 'dictionary' /* required */
+    };
+    dynamoDb.describeTable(params, function(err, data) {
+      if (err) console.log(err, err.stack); // an error occurred
+      else     console.log('Table existed', data);           // successful response
+    });
+  }
 
-
-  });
-}, 1000);
+});

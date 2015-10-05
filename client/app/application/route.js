cd00ccd468f0bea3556191560e4ae8008db4ef84
tyleriguchi <tyleriguchi@gmail.com>
2015-10-04 17:58:41 -0700

    use custom authenticator

--- client/app/application/route.js
+++ client/app/application/route.js
@@ -12,11 +12,11 @@ export default Ember.Route.extend(ApplicationRouteMixin, {
     sessionRequiresAuthentication(){
       // Check out the docs for all the options:
       // https://auth0.com/docs/libraries/lock/customization
-
+      console.log('require')
       // These options will request a refresh token and launch lock.js in popup mode by default
       var lockOptions = {authParams:{scope: 'openid'}};
 
-      this.get('session').authenticate('simple-auth-authenticator:lock', lockOptions);
+      this.get('session').authenticate('authenticator:dictionary-authenticator', lockOptions);
     },
     invalidateSession() {
       return this.get('session').invalidate().then( () => {

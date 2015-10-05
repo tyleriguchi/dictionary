bfbb4ad8f0159001ca5465caa430810d7ef56a7c
tyleriguchi <tyleriguchi@gmail.com>
2015-10-04 17:52:17 -0700

    postgres seed data

---
+++ server/db/seeds/users.js
@@ -0,0 +1,32 @@
+
+exports.seed = function(knex, Promise) {
+  return Promise.join(
+    // Deletes ALL existing entries
+    knex('users').del(),
+
+    // Inserts seed entries
+    knex('users').insert(
+      {
+        id: 1,
+        email: 'jaimielover@gmailcom',
+        created_at: new Date()
+      }
+    ),
+
+    knex('users').insert(
+      {
+        id: 2,
+        email: 'tyrionthegiant@gmail.com',
+        created_at: new Date()
+      }
+    ),
+
+    knex('users').insert(
+      {
+        id: 3,
+        email: 'jonsnow@gmail.com',
+        created_at: new Date()
+      }
+    )
+  );
+};

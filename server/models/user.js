db89d943cf3d675b48a173c3f5b9bc7423de2417
tyleriguchi <tyleriguchi@gmail.com>
2015-10-06 16:45:48 -0700

    better conventions on exporting model

--- server/models/user.js
+++ server/models/user.js
@@ -1,9 +1,14 @@
-var bookshelf = require('../libs/Bookshelf');
+var Bookshelf = require('../libs/Bookshelf');
 var Words = require('./word');
 
-module.exports = bookshelf.Model.extend({
+exports.User = Bookshelf.Model.extend({
   tableName: 'users',
+  
   words: function() {
     return this.hasMany(Words);
   }
 });
+
+// exports.Users = Bookshelf.Collection.extend({
+//   model: User
+// })

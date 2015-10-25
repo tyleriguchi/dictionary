9a1567fc0ee010fc37bcc96c57f1228bc242aca4
tyleriguchi <tyleriguchi@gmail.com>
2015-10-25 15:10:31 -0700

    remove pg deps

--- server/libs/Bookshelf.js
+++
@@ -1,5 +0,0 @@
-var knexConfig = require('../knexfile');
-var settings = require('../config/settings');
-var knex = require('knex')(knexConfig[settings.environment]);
-
-module.exports = require('bookshelf')(knex);
